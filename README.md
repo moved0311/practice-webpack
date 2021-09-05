85b307628e19794f2e99942bccb000d965dadcaa
taiyi <god830311@gmail.com>
2021-09-06 03:20:11 +0800

    practice webpack

---
+++ README.md
@@ -0,0 +1,4 @@
+## Reference
+
+- [Webpack](https://webpack.js.org/)
+- [【前端速成】Webpack5 快速入門｜ Tiktok 工程師帶你入門前端｜布魯斯前端](https://www.youtube.com/watch?v=uP6KTupfyIw)
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[hash].bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+        type: 'javascript/auto',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.[hash].css',
+    }),
+  ],
+}
